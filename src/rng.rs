ff1ef260556bb885691d0d6b43709a3687573ecd
Wez Furlong <wez@wezfurlong.org>
2025-04-12 21:29:32 -0700

    tidy up rng handling

--- src/rng.rs
+++ src/rng.rs
@@ -6,7 +6,9 @@ use embassy_sync::mutex::Mutex;
 use embassy_sync::once_lock::OnceLock;
 use rand_chacha::ChaCha20Rng;
 use rand_chacha::rand_core::SeedableRng;
+use rand_core::RngCore;
 use static_cell::StaticCell;
+use sunset::random::{CryptoRngProvider, EphemeralSecret, SigningKey};
 
 static RNG: OnceLock<Mutex<CriticalSectionRawMutex, Trng<TRNG>>> = OnceLock::new();
 
@@ -23,11 +25,8 @@ pub fn init_rng(trng: TRNG) {
     }
 
     // Stash a reference for sunset to use
-    static SUNSET_RNG: StaticCell<ChaCha20Rng> = StaticCell::new();
-    let rng = SUNSET_RNG.init_with(|| {
-        let mut rng = WezTermRng;
-        ChaCha20Rng::from_rng(&mut rng).expect("failed to init ChaCha20Rng")
-    });
+    static SUNSET_RNG: StaticCell<RngProvider> = StaticCell::new();
+    let rng = SUNSET_RNG.init_with(|| RngProvider);
     unsafe {
         sunset::random::assign_rng(rng);
     }
@@ -52,5 +51,31 @@ impl rand_core::RngCore for WezTermRng {
             .try_lock()
             .unwrap()
             .try_fill_bytes(buf)
+    }
+}
+
+struct RngProvider;
+impl RngProvider {
+    fn make_rng(&self) -> Result<ChaCha20Rng, sunset::Error> {
+        let mut rng = WezTermRng;
+        ChaCha20Rng::from_rng(&mut rng)
+            .map_err(|_| sunset::Error::msg("failed to init ChaCha20Rng"))
+    }
+}
+impl CryptoRngProvider for RngProvider {
+    fn fill_random(&self, buf: &mut [u8]) -> Result<(), sunset::Error> {
+        let mut rng = self.make_rng()?;
+        rng.fill_bytes(buf);
+        Ok(())
+    }
+
+    fn make_ephemeral_secret(&self) -> Result<EphemeralSecret, sunset::Error> {
+        let mut rng = self.make_rng()?;
+        Ok(EphemeralSecret::random_from_rng(&mut rng))
+    }
+
+    fn make_signing_key(&self) -> Result<SigningKey, sunset::Error> {
+        let mut rng = self.make_rng()?;
+        Ok(SigningKey::generate(&mut rng))
     }
 }
